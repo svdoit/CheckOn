3da04da724b37104b95d3e6ede8f1c3b085a38c5
sungmin <icechocola05@gmail.com>
2020-10-09 00:05:00 +0900

    Item 모델 생성 & static
    -Item 모델에 이름, 이미지, 가격, 카테고리 항목을 넣었습니다.
    -static으로 이미지를 넣을 수 있는 환경을 만들었습니다.

--- checkon-project/checkon/models.py
+++ checkon-project/checkon/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    itemName = models.CharField(max_length=200)
+    itemImg = models.ImageField(upload_to='images/', blank=True)
+    itemPrice = models.CharField(max_length=20)
+    category = models.IntegerField(max_length=10)
+
+# 0. 과일, 견과
+# 1. 수산
+# 2. 정육, 계란
+# 3.채소
+# 4.쌀잡곡
+# 5.유제품
+# 6.반찬, 간편식
+# 7.액체류(생수, 음료, 주류)
+# 8.과자, 빵
+# 9.즉석조리(라면, 통조림, 소스장류)
+# 10.건강식품
